563d93b23c732cfcbc8fbfefa04bdbd4831de784
Eric Kemp-Benedict <eric.kemp-benedict@sei.org>
2022-09-08 13:12:07 -0400

    Replaced option "-h" since that is reserved for help

--- scripts/runleapmacro.jl
+++ scripts/runleapmacro.jl
@@ -30,14 +30,10 @@ function parse_commandline()
             help = "initial run number"
 			arg_type = Int64
             default = 0
-        "--max-runs", "-m"
-            help = "maximum number of runs of LEAP"
-			arg_type = Int64
-            default = 1
         "--report-diagnostics", "-d"
             help = "report diagnostic information"
             action = :store_true
-        "--hide-leap", "-h"
+        "--suppress-leap", "-s"
             help = "hide LEAP while running"
             action = :store_true
         "--final-year", "-y"
@@ -55,10 +51,13 @@ cd(@__DIR__)
 parsed_args = parse_commandline()
 cfg_file = parsed_args["config_file"]
 cfg_yaml = YAML.load_file(cfg_file)
+# These must be set to these values for the script to work
+cfg_yaml["model"]["run_leap"] = true
+cfg_yaml["model"]["max_runs"] = 1
+# Get options from command line
 cfg_yaml["output_folder"] = parsed_args["scenario"]
 cfg_yaml["LEAP-info"]["scenario"] = parsed_args["scenario"]
-cfg_yaml["model"]["max_runs"] = parsed_args["max_runs"]
-cfg_yaml["model"]["hide_leap"] = parsed_args["hide_leap"]
+cfg_yaml["model"]["hide_leap"] = parsed_args["suppress_leap"]
 cfg_yaml["years"]["end"] = parsed_args["final_year"]
 cfg_yaml["report-diagnostics"] = parsed_args["report_diagnostics"]
 if parsed_args["init_run_number"] != 0
